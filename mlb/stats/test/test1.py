756231da05bcb435eccab3f09e5d3d6d866dad25
ragoo80 <ragoo80@gmail.com>
2018-09-04 16:00:19 +0900

    add test lambda function, find element in list

--- mlb/stats/test/test1.py
+++ mlb/stats/test/test1.py
@@ -11,3 +11,42 @@ from django.test import TestCase
 #
 # test()
 # print("after test", a)
+
+
+
+# to find value in list test ------------------------------------------------------------
+StrList = ['x','t','ab','ssd']
+# print StrList.index('t')
+# print StrList.index('tsd') --> ValueError: u'tsd' is not in list
+
+NumList = [33,55,15,125,36254]
+result = NumList.index(55) if 55 in NumList else 'there is not 33'
+
+
+# using for lambda function ------------------------------------------------------------
+# 람다는 한 줄로 함수를 작성하는 방법인데 식 형태로 되어 있다고 해서 람다 표현식(lambda expression)이라고 부른다.
+# 람다는 함수를 간편하게 작성할 수 있어서 다른 함수의 인수로 넣을 때 주로 사용.
+
+# 람다 적용 전
+def plusFn(x) :
+    return x + 10
+# result = list( map(plusFn, [1,2,3]) )
+
+# 람다로 적용 후
+result = map( lambda x: x+10, [1,2,3] )
+
+
+# to find an element in a string array use ------------------------------------------------------------
+# case by use lamda expression code
+strlist = ["Moon","Earth","Jupiter","Neptune","Earth","Venus"]
+get_indexes = lambda target, list: [index for (value, index) in zip(list, range(len(list))) if target == value]
+print(get_indexes("Earth",strlist))
+
+# case by just use normal function expression code
+# strlist = ["Moon","Earth","Jupiter","Neptune","Earth","Venus"]
+# def testFn(target, list) :
+#     for (y, i) in zip( list, range(len(list)) ) :
+#         # print zip( list, range(len(list)) )
+#         # print 'y is : ', y , 'and i is : ', i
+#     return [ index for (value, index) in zip( list, range(len(list)) ) if target == value ]
+# testFn("Earth",strlist)
