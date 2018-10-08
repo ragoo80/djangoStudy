3f445c004bc744becb5470394e53c3f64e2cfb8b
ragoo80 <ragoo80@gmail.com>
2018-10-08 19:45:43 +0900

    add carNumber validator

--- mlb/stats/test/timeTest.py
+++ mlb/stats/test/timeTest.py
@@ -14,21 +14,57 @@
 
 
 
-from urllib import urlopen
-import urllib2, urllib
-import requests
+# from urllib import urlopen
+# import urllib2, urllib
+# import requests
+#
+# def imgLoaded(url) :
+# 	req = requests.get(url.decode('utf-8'))
+# 	if req.status_code == 200:
+# 		print 'img yes'
+# 		data = urllib.urlopen(req)
+# 		print data
+# 		dr = data.read()
+# 		print dr
+# 	else :
+# 		raise Exception("can't load html!!!!!!!!!!!!!!!")
+#
+# testUrl = 'https://www.lotterentacar.net/atch/upload/getImage.do?atchFileId=20170205teramin47@naver.com06하5928frontImg_V.jpg&fileSepar=succession'
+#
+# imgLoaded(testUrl)
 
-def imgLoaded(url) :
-	req = requests.get(url.decode('utf-8'))
-	if req.status_code == 200:
-		print 'img yes'
-		data = urllib.urlopen(req)
-		print data
-		dr = data.read()
-		print dr
+
+# --------------------------------------------------------------------------------------------------------------
+# 자동차번호 7자리 번호 보다 작거나, 영문 포함
+import re
+pagingList = [
+	# u"goDetail('68\ud5583435','chs1972');",
+	u"goDetail('46\ud6385532','park2968@naver.com');",
+	u"goDetail('66\ud638','abukki4');",
+	u"goDetail('bmw320d','tissue7587@naver.com');",
+	u"goDetail('50gk7294','trade75');",
+	u"goDetail('44\ud558 4674','6aksmef');",
+	u"goDetail('52\ud63813ㅂ8','sdh0328@naver.com');",
+	u"goDetail('03하9000.','leecj1210');"
+]
+
+pattern = re.compile('[a-z|A-Z|ㄱ-]+')
+for idx in range(len(pagingList)) :
+	carNumber = pagingList[idx].split("'")[1].replace(' ','').replace('.','')
+	print carNumber
+	print len(carNumber)
+	print re.search(pattern, carNumber.encode('utf-8') )
+	if len(carNumber) != 7 or re.search(pattern, carNumber.encode('utf-8') ) != None:
+		print '크롤링에 포함 안시킴 '
 	else :
-		raise Exception("can't load html!!!!!!!!!!!!!!!")
+		print '크롤링에 포함 시킴 '
+	print '------------------------'
 
-testUrl = 'https://www.lotterentacar.net/atch/upload/getImage.do?atchFileId=20170205teramin47@naver.com06하5928frontImg_V.jpg&fileSepar=succession'
 
-imgLoaded(testUrl)
+# print re.search(pattern, u"66\ud638")
+# print re.search(pattern, u"03하9000")
+# print re.search(pattern, "03하9000" )
+# print re.search(pattern, "52호13ㅂ8" )
+
+
+
